958c7864f03dcdc16f9212f9b0e7ffa6f1ad0d79
Đặng Gia Khiêm <khiem192704@gmail.com>
2026-09-13 23:12:30 +0700

    Vector

--- include/DSA/vector.h
+++ include/DSA/vector.h
@@ -24,23 +24,24 @@ class Vector
         }
     public:
         Vector() : data(nullptr), size(0), capacity(0){}
-        explicit Vector(int initSize) : size(initSize), capacity(initSize){
+        Vector(int initSize) : size(initSize), capacity(initSize){
             data = new T[capacity];
             for(int i = 0; i < size; i++){
-                data = T();
+                data[i] = T();
             }
         }
         Vector(const Vector& other) : size(other.size), capacity(other.capacity){
-            data = new T[capacity];
+            data = new T[size];//capacity
             for(int i = 0; i < size; i++){
                 data[i] = other.data[i];
             }
         }
         ~Vector(){  delete[] data; }
-        Vector& operator=(const Vector& other) noexpect {
-            if(this != other){
+        //(a = b) = c
+        Vector& operator=(const Vector& other) {
+            if(this != &other){
                 delete[] data;
-                data = new T[other.capacity];
+                data = new T[other.size];
                 size = other.size;
                 capacity = other.capacity;
                 for(int i = 0; i < size; i++){
@@ -49,65 +50,58 @@ class Vector
             }
             return *this;
         }
-        T& operator[](int index) noexpect {
+        T& operator[](int index) {
             return data[index];
         }
-        const T& operator[](int index) const noexpect{
+        const T& operator[](int index) const{
             return data[index];
         }
-        T& at(int index){
-            if(index < 0 || index >= size){ throw out_of_range"Vector index out of range";}
-            return data[index];
-        }
-        const T& at(int index) const{
-            if(index < 0 || index >= size){ throw out_of_range"Vector index out of range";}
-            return data[index];
-        }
-        T& get(int index){ return at(index); }
-        const T& get(int index) const{ return at(index); }
+        // T& at(int index){
+        //     if(index < 0 || index >= size){ throw out_of_range("Vector index out of range");}
+        //     return data[index];
+        // }
+        // const T& at(int index) const{
+        //     if(index < 0 || index >= size){ throw out_of_range("Vector index out of range");}
+        //     return data[index];
+        // }
         
-        bool empty() const noexpect {
+        bool empty() const {
             return size == 0;
         }
         
-        bool is_empty() const {
-            return empty();
-        }
-        
         T& front(){
-            if (isEmpty()) {
+            if (empty()) {
                 throw out_of_range("Vector is empty");
             }
             return data[0];
         }
         
         const T& front() const{
-            if (isEmpty()) {
+            if (empty()) {
                 throw out_of_range("Vector is empty");
             }
             return data[0];
         }
         
         T& back(){
-            if (isEmpty()) {
-                throw out_of_range("Vector is empty");
-            }
-            return data[sizze - 1];
-        }
-        
-        const T& back() const{
-            if (isEmpty()) {
+            if (empty()) {
                 throw out_of_range("Vector is empty");
             }
             return data[size - 1];
         }
-        T* getData(){   return data; }
-        const T* getData() const {   return data; }
-        int size() const noexpect{
+        
+        const T& back() const{
+            if (empty()) {
+                throw out_of_range("Vector is empty");
+            }
+            return data[size - 1];
+        }
+        
+        int size() const{
             return size;
         }
-        int capacity() const noexpect{
-            return capacity
+        int capacity() const{
+            return capacity;
         }
         
         T* begin(){
@@ -122,16 +116,14 @@ class Vector
         const T* end() const {
             return data + size;
         }
-        void push_back(const &T value){
+        void push_back(const T& value){
             if(size == capacity){
                 resize();
             }
             data[size++] = value;
         }
-        void add(const &T value){
-            push_back(value);
-        }
-        void insert(int index, const &T value){
+        
+        void insert(int index, const T& value){
             if(index < 0 || index > size){
                 throw out_of_range("Insert index out of range");
             }
@@ -140,11 +132,11 @@ class Vector
             data[index] = value;
             size++;
         }
-        void push_front(const &T value){
+        /*void push_front(const T& value){
             insert(0, value);
-        }
+        }*/
         void pop_back(){
-            if(isEmpty()){
+            if(empty()){
                 throw out_of_range("Vector is empty");
             }
             size--;
@@ -156,12 +148,12 @@ class Vector
             for(int i = index; i < size - 1; i++) data[i] = data[i + 1];
             size--;
         }
-        void pop_front(){
-            if(isEmpty()){
+        /*void pop_front(){
+            if(empty()){
                 throw out_of_range("Vector is empty");
             }
             erase(0);
-        }
+        }*/
         void remove(int index){
             erase(index);
         }
@@ -176,14 +168,14 @@ class Vector
             data = new_data;
             capacity = new_capacity;
         }
-        void resize(int new_size){
+        void reNewsize(int new_size){
             if(new_size >capacity){
                 reserve(new_size);
             }
             for(int i = size; i < new_size; i++) data[i] = T();
             size = new_size;
         }
-        int find(const &T value){
+        /*int find(const T& value){
             for(int i = 0; i < size; i++){
                 if(data[i] == value) return i;
             }
@@ -191,7 +183,7 @@ class Vector
         }
         bool contains(const T& value) const {
             return find(value) != -1;
-        }
+        }*/
         void fit_capacity(){
             if(capacity > size){
                 T* new_data = new T[size];
